073851cd279bbad4478735c1f9b74eee0a4d67a6
Nikolai <agro034@mail.ru>
2023-03-06 13:40:57 +0300

    Задача 60

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+int sizeX = SetNumber("размер X");
+int sizeY = SetNumber("размер Y");
+int sizeZ = SetNumber("размер Z");
+int[,,] matrix60 = CubeMatrix(sizeX, sizeY, sizeZ);
+Print3dMatrix(matrix60);
+
+int SetNumber(string numberName)
+{
+    Console.Write($"Enter number {numberName}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+
+void PutOrder(int[] Numbers)  //  Метод заполнение одномерного массива по порядку
+{
+    int length = Numbers.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        Numbers[i] = i + 10;
+    }
+    return;
+}
+
+int[] Shuffle(int[] arr)  // Метод для перемешивания чисел
+{
+    Random rand = new Random();
+
+    for (int i = arr.Length - 1; i >= 1; i--)
+    {
+        int j = rand.Next(i + 1);
+
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+int[,,] CubeMatrix(int valueX, int valueY, int valueZ)  // Метод для заполнения матрицы неповторяющимися случайными целыми числами от 10 до 99
+{
+    int[,,] matrix = new int[valueX, valueY, valueZ];
+
+    if ((valueX * valueY * valueZ) > 90) Console.WriteLine("Слишком большой размер. Количество значений не должно превышать 90");
+    int[] array = new int[90];
+    PutOrder(array);
+    Shuffle(array);
+
+    for (int i = 0; i < valueX * valueY * valueZ;)
+    {
+        for (int x = 0; x < valueX; x++)
+        {
+            for (int y = 0; y < valueY; y++)
+            {
+                for (int z = 0; z < valueZ; z++)
+                {
+                    matrix[x, y, z] = array[i];
+                    i++;
+                }
+
+            }
+        }
+    }
+    return matrix;
+}
+
+void Print3dMatrix(int[,,] matrix)  //  Метод для вывода в консоль 3d матрицы
+{
+    for (int x = 0; x < matrix.GetLength(0); x++)
+    {
+        for (int y = 0; y < matrix.GetLength(1); y++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                System.Console.Write($"{matrix[x, y, z]} ({x}, {y}, {z}) ");
+            }
+            System.Console.WriteLine();
+        }
+    }
+}
